5174c3fff369b7d70faff604a81f37c90d365ec8
bluszcz <bluszcz@bluszcz.net>
2019-11-01 01:25:09 +0100

    Version 0.9.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pyqttotp',
-    version='0.9.0',
+    version='0.9.1',
     packages=['pyqttotp'],
     include_package_data=False,
     license='Apache License version 2.0',
